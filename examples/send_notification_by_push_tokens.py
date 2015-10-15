0c3f22740fd688fbf555a7cc6a43e29d893407e0
Konstantin Misyutin <ikeeip@arello-mobile.com>
2015-10-15 17:49:58 +0600

    add examples dir with first example

---
+++ examples/send_notification_by_push_tokens.py
@@ -0,0 +1,19 @@
+from pypushwoosh.client import PushwooshClient
+from pypushwoosh.command import CreateMessageForApplicationCommand
+from pypushwoosh.notification import Notification
+
+AUTH_TOKEN = 'AUTH_TOKEN'
+APPLICATION_CODE = 'APP-CODE'
+
+notification = Notification()
+notification.content = 'Hello world!'
+notification.devices = [
+    'PUSH_TOKEN_0',
+    'PUSH_TOKEN_1',
+]
+
+command = CreateMessageForApplicationCommand(notification, APPLICATION_CODE)
+command.auth = AUTH_TOKEN
+
+client = PushwooshClient()
+response = client.invoke(command)
